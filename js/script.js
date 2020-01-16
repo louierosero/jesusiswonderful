d1f1d4ea82f6cf144123492eb753a98f8fc5982c
Louie Rosero <roserolouie@gmail.com>
2020-01-16 19:32:05 +0800

    Homepage Layout [Modified]

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ var mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
   direction: 'horizontal',
   loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
