5b7605f6e8aa896b66e9ba1cf54434b5c8074c13
Louie Rosero <roserolouie@gmail.com>
2020-02-24 15:19:00 +0800

    Homepage Layout - Slider [Adding the AUTOPLAY functionality]

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 'auto',
   direction: 'horizontal',
   loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
